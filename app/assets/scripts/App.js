400ac83e3640df494d8d76703756d52a7c6ef8a5
Mohd Sharfuddin Khan <khansharfuddin7@gmail.com>
2020-01-26 18:56:11 +0530

    completed the modal

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,11 +3,27 @@ import MobileMenu from "./module/MobileMenu";
 import RevealOnScroll from "./module/RevealOnScroll";
 import StickyHeader from "./module/StickyHeader";
 
-let stickyHeader = new StickyHeader();
+new StickyHeader();
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
+new MobileMenu();
+let modal;
 
-let mobileMenu = new MobileMenu();
+document.querySelectorAll(".open-modal").forEach(el => {
+  el.addEventListener("click", e => {
+    e.preventDefault();
+    if (typeof modal == "undefined") {
+      import(/*webpackChunkName: "modal"*/ "./module/modal")
+        .then(x => {
+          modal = new x.default();
+          setTimeout(() => modal.openTheModal(), 20);
+        })
+        .catch(() => console.log("thre was a problem."));
+    } else {
+      modal.openTheModal();
+    }
+  });
+});
 
 if (module.hot) {
   module.hot.accept();
